12cbc2e6b5328d394fff9cc7ab0c9f5ac46c66e2
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-08-24 16:24:39 -0300

    fix: update command dispatch

--- lambda.sh
+++ lambda.sh
@@ -5,35 +5,36 @@
 
 usage()
 {
-  echo "Usage: lambda <command> [arguments]..."
-  echo "Minimalist declarative package manager"
-  echo ""
-  echo "Commands:"
-  echo "  mutate <append|purge> <package>"
-  echo "      Modify the desired system state."
-  echo ""
-  echo "  reconcile"
-  echo "      Reconcile the system with the desired state."
-  echo ""
-  echo "  --help"
-  echo "      Display this help message."
+    echo "Usage: lambda <command> [arguments]..."
+    echo "Minimalist declarative package manager"
+    echo ""
+    echo "Commands:"
+    echo "  mutate <append|purge> <package>"
+    echo "      Modify the desired system state."
+    echo ""
+    echo "  reconcile"
+    echo "      Reconcile the system with the desired state."
+    echo ""
+    echo "  --help"
+    echo "      Display this help message."
 }
 
 if [ "$#" -eq 0 ]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 if [ "$1" = "--help" ]; then
-  usage
-  exit 0
+    usage
+    exit 0
 elif [ "$1" = "reconcile" ]; then
-  lambda_reconcile
-  exit $?
+    lambda_reconcile
+    exit $?
 elif [ "$1" = "mutate" ]; then
-  lambda_mutate "$2" "$3"
-  exit $?
+    shift
+    lambda_mutate "$@"
+    exit $?
 else
-  usage
-  exit 0
+    usage
+    exit 0
 fi
