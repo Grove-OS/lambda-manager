692898734466b3f8dd01d52b426887e1115d5824
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-08-23 23:29:43 -0300

    feat: add mutate command

--- lambda.sh
+++ lambda.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-. /usr/lib/lambda/reconcile.sh
+. /usr/lib/lambda/lambda_reconcile.sh
+. /usr/lib/lambda/lambda_mutate.sh
 
 usage()
 {
-    echo "Usage: lambda [OPTION]..."
-    echo "Minimalist package manager"
+    echo "Usage: lambda <command> [arguments]..."
+    echo "Minimalist declarative package manager"
     echo ""
-    echo "--help      Displays this menu"
-    echo "reconcile   Apply the changes to system"
+    echo "Commands:"
+    echo "  mutate <append|purge> <package>"
+    echo "      Modify the desired system state."
+    echo ""
+    echo "  reconcile"
+    echo "      Reconcile the system with the desired state."
+    echo ""
+    echo "  --help"
+    echo "      Display this help message."
 }
 
 if [ "$#" -eq 0 ]; then
@@ -20,6 +28,9 @@ if [ "$1" = "--help" ]; then
     usage
     exit 0
 elif [ "$1" = "reconcile" ]; then
-    reconcile
-    exit 0
+    lambda_reconcile
+    exit $?
+elif [ "$1" = "mutate" ]; then
+    lambda_mutate "$2" "$3"
+    exit $?
 fi
